ee8db93e4b3f301922046a2c1688d72ae11ba620
Mike Carson <mikecarson72020@gmail.com>
2024-07-23 14:00:46 -0400

    Replaced HTML table with datatables.net and styled it.
    #7

--- package/exercise1/ui/src/app/astronaut-results/astronaut-results.component.ts
+++ package/exercise1/ui/src/app/astronaut-results/astronaut-results.component.ts
@@ -1,16 +1,17 @@
 import { Component, inject, Input, Output, EventEmitter, SimpleChanges, OnChanges } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { RouterOutlet, RouterModule } from '@angular/router';
 import { Astronaut } from '../models/astronaut.model';
 import { AstronautDetailsComponent } from '../astronaut-details/astronaut-details.component';
 import { AstronautService } from '../services/astronaut-service/astronaut.service';
 import { AstronautDutyDto } from '../models/astronaut-duty-dto.model';
+import { DataTablesModule } from 'angular-datatables';
 
 @Component({
   selector: 'app-astronaut-results',
   standalone: true,
-  imports: [CommonModule, FormsModule, RouterOutlet, RouterModule, AstronautDetailsComponent],
+  imports: [CommonModule, FormsModule, RouterOutlet, RouterModule, AstronautDetailsComponent, DataTablesModule],
   templateUrl: './astronaut-results.component.html',
   styleUrl: './astronaut-results.component.css'
 })
@@ -21,6 +22,9 @@ export class AstronautResultsComponent implements OnChanges {
   @Input() searchName?: string;
   @Output() isBusy = new EventEmitter<boolean>();
   @Output() astronautDutyAdded = new EventEmitter<number>();
+
+  dtOptions = {};
+  datePipe: DatePipe = new DatePipe('en-US');
 
   newAstronautDutyFormVisible: boolean = false;
   astronautResultList: Astronaut[] = [];
@@ -46,14 +50,56 @@ export class AstronautResultsComponent implements OnChanges {
   loadAllAstronauts(): void {
     this.isBusy.emit(true);
     if (this.searchName === null || this.searchName === undefined) {
-      this.astronautService.getAllAstronauts()
-        .subscribe({
-          next: (res) => {
-            this.astronautResultList = res.people;
+      this.dtOptions = {
+        processing: true,
+        lengthMenu: [5, 10, 25],
+        ajax: (dataTableParameters: any, callback: any) => {
+          this.astronautService.getAllAstronauts()
+            .subscribe({
+              next: (res) => {
+                this.astronautResultList = res.people;
+                callback({
+                  recordsTotal: res.people.length,
+                  recordsFiltered: res.people.length,
+                  data: res.people
+                })
+              },
+              error: (e) => console.error(e),
+              complete: () => this.isBusy.emit(false)
+            });
+        },
+        columns: [
+          { title: "Astronaut", data: "name" },
+          { title: "Rank", data: "currentRank" },
+          {
+            title: "Duty Title",
+            data: "currentDutyTitle",
+            render: (data: any, type: string, row: any, meta: object) => {
+              return row.currentDutyTitle;
+            }
           },
-          error: (e) => console.error(e),
-          complete: () => this.isBusy.emit(false)
-        });
+          { title: "Career Start Date", data: "careerStartDate", searchable: false, ngPipeInstance: this.datePipe, ngPipeArgs: ['MM/dd/yyyy'] },
+          { title: "Career End Date", data: "careerEndDate", searchable: false, ngPipeInstance: this.datePipe, ngPipeArgs: ['MM/dd/yyyy'] }
+        ],
+        rowCallback: (row: Node, data: any[] | Object, index: number) => {
+          // Unbind first in order to avoid any duplicate handler
+          $('td', row).off('click');
+          $('td', row).on('click', () => {
+            this.showNewAstronautDutyForm(<Astronaut>data);
+          });
+          return row;
+        },
+        select: {
+          style: 'single',
+          info: false
+        },
+        language: {
+          entries: {
+            _: 'astronauts',
+            1: 'astronaut'
+        }
+        }
+      }
     } else {
       console.log(`Searching for: ${this.searchName}`);
       this.astronautService.getAstronautByName(encodeURIComponent(this.searchName))
